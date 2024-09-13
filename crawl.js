d14cca2c229a669c4556db703e58a906fa73e8d8
shivansh <trivedishivansh1207@gmail.com>
2024-09-13 18:40:23 +0530

    Crawl and Tests added

---
+++ crawl.js
@@ -0,0 +1,9 @@
+function normalizeURL(url) {
+    const urlObj = new URL(url)
+    let fullPath = `${urlObj.host}${urlObj.pathname}`
+    if (fullPath.slice(-1) === '/') {
+      fullPath = fullPath.slice(0, -1)
+    }
+    return fullPath
+}
+export {normalizeURL};
