0eddc6156a004b0d76f2c24188cd3602e6cc984e
shivansh <trivedishivansh1207@gmail.com>
2024-09-16 23:36:53 +0530

    Basic version created

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
 import { crawlPage } from "./crawl.js";
-import { printReport } from "./report.js";
+import { sortPagesByCount } from "./sort.js";
 import { generateCSVFromPages} from "./csv.js";
+import { sendEmailWithAttachment } from "./email.js";
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 async function main(){
     const args = process.argv.slice(2);
-    if(args.length>1){
+    if(args.length!==2){
         console.log('Error');
         process.exit(1);
     }
-    else if(args.length<1){
-        console.log('Error');
-        process.exit(1);
-    }
-    else{
-        console.log('Crawler Started!!!');
-        const pages = await crawlPage(args);
-        printReport(pages);
+    const [url, email] = args;
+    console.log('Crawler Started!!!');
+    try{ 
+        let pages = await crawlPage(url);
+        pages = sortPagesByCount(pages);
         await generateCSVFromPages(pages);
+        const csvFilePath = path.join(__dirname, 'output.csv');
+        await sendEmailWithAttachment(email,csvFilePath);
         process.exit(0);
+    }catch(error){
+        console.error('Error during crawling process:', error);
     }
 }
 main();
